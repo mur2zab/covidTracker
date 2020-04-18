416b675589464eecd709c62d9c638b143c2eb0a0
Murtuza.B <murtuzabagasrawala@gmail.com>
2020-04-18 19:34:19 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Router from './router';
+import BottomNavigation from './src/screens/BottomNavigation';
+
+export default function App() {
+  return (
+    <Router />
+    // <BottomNavigation />
+  );
+}
